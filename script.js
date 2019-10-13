5378b1286ae1b21366014b9bda5e007fdd5e0659
AlphieKiu <kiuhuaien87@gmail.com>
2019-10-12 22:48:30 -0500

    added window.onload init(). added fetch and URL. added responsecode.

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function init() {
+    fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+        response.json().then(function(json) {
+            let div = document.getElementById("comments")
+            for (let i = 0; i < json.length; i++) {
+                div.innerHTML += `
+                
+            `
+            }
+        });
+    });
+    // Then code code for adding the comment JSON values to the <div id="comments"> div using a for loop
+}
+
+window.onload = init;
+
 // Write your JavaScript code here!
 
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
